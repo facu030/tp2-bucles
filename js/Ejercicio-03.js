2c782bd2eb12d08934430474dc38924b496716f9
Facundo Martinez Rodriguez <facundomartinezr03@gmail.com>
2024-08-16 12:13:16 -0300

    agregue cambios al ejer3

--- js/Ejercicio-03.js
+++ js/Ejercicio-03.js
@@ -1,14 +1,15 @@
 // 3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
-let cadena;
-while (true) {
-  let confirma = confirm(
-    "introducir cadenas de texto prewsione aceptar cancelar para salir"
-  );
 
-  if (confirma == true) {
-    document.write("-" + cadena);
-    console.log(cadena)
-  } else if (confirma == false) {
-    let cadena = prompt("ingrese cadenas de texto");
+let resultado = "";
+do {
+  let text = prompt("ingrese palabras");
+  if (resultado == "") {
+    //guardo la cadena en resultado y despues concateno con resultado mas el ultimo texto ingresado
+    resultado = text;
+  } else {
+    resultado = resultado + "-" + text;
   }
-}
+} while (confirm("Desea continuar?"));
+
+document.write(resultado);
+console.log(resultado);
